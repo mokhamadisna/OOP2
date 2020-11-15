f0140cccf1575c753f9dd24b8ecef64d64d08bbd
mokhamadisna <mokhammadisna@gmail.com>
2020-11-15 16:28:31 +0700

    publish

---
+++ src/com/isna/basic/Main.java
@@ -0,0 +1,14 @@
+package com.isna.basic;
+
+import com.isna.basic.music.Gitar;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        Gitar.bunyi();
+
+        
+    }
+}
+
---
+++ src/com/isna/basic/music/Gitar.java
@@ -0,0 +1,7 @@
+package com.isna.basic.music;
+
+public class Gitar {
+    public static void bunyi(){
+        System.out.println("jrenggg..");
+    }
+}
